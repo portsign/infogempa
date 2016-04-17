c3a6a2b05e77d654a30f68a041105573089eeb00
indocreator <portsign@gmail.com>
2016-04-17 22:37:47 +0700

    secure file get content part 2

--- src/Controller/BelajarController.php
+++ src/Controller/BelajarController.php
@@ -8,7 +8,8 @@ class BelajarController extends AppController
 	public function index()
     {
 
-    	function getSslPage($url) {
+    	function getSslPage($url) 
+    	{
 		    $ch = curl_init();
 		    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 		    curl_setopt($ch, CURLOPT_HEADER, false);
@@ -21,12 +22,12 @@ class BelajarController extends AppController
 		    return $result;
 		}
 
-        $content = getSslPage('https://infogempa.com/artikel/api/get_recent_posts/');
+        $content = getSslPage('http://infogempa.com/artikel/api/get_recent_posts/');
         $jsons = json_decode($content);
 
         $title = 'Info Gempa Dunia | Belajar Geologi dan Geofisika | infogempa.com';
         $meta_desc = 'Info Gempa | Kami menyediakan informasi mengenai gempa bumi yang terjadi di dunia. untuk mendapatkan informasi secara update silahkan subscribe';
         $this->set(compact('jsons','title','meta_desc'));
-        $this->set('_serialize', ['gempa']);
+        $this->set('_serialize', ['gempa', 'jsons']);
     }
 }
